254e189c59c4f4db6c21f4fa4311f0bbcf6ffa6f
Jamesbeta12 <jamesdanielbetancurt459@gmail.com>
2023-06-21 18:05:39 -0500

    James

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,6 +1,7 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import React from 'react';
 import LogoutButton from '../LogOut/LogoutButton';
+import './profile.css'
 
 
 const Profile = () => {
@@ -13,13 +14,13 @@ const Profile = () => {
     return (
         isAuthenticated && (
             <article className='column'>
-                <img src={user.picture} alt={user.name}/>
-                <h2>{user.name}</h2>
+                <img className='foto' src={user.picture} alt={user.name}/>
+                <h2 className='correo'>{user.name}</h2>
                 <p>{user.email}</p>
                 {/* <ul>
                     {Object.keys(user).map((objKey, i) => <li key={i}>{objKey}: {user[objKey]} </li>)}
                 </ul> */}
-                          <LogoutButton />
+                        <LogoutButton />
             </article>
         )
     )
